82be47c03f20c711db9e17a9ce57ef551caacc5d
ksafranski <ksafranski@gmail.com>
2026-01-19 14:53:04 -0600

    chore(theme): css injections

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
@@ -12,6 +13,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    libInjectCss(),
     dts({
       insertTypesEntry: true,
       tsconfigPath: './tsconfig.json',
